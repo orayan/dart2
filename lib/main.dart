0c0f42815aca8c2825e0436534269704ab8d9273
Administrator <admin@example.com>
2022-03-18 16:54:24 +0200

    second project

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main()  => const RunApp();
+
+class RunApp extends StatelessWidget {
+  const RunApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Book Shop',
+      debugShowCheckedModeBanner: false,
+      home: ,
+    );
+  }
+}
